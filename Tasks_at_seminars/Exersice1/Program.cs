c3909e77337bf373c085fed01ddc606cc3e33821
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-09 12:25:43 +0300

    Add first exr, replace variable

---
+++ Tasks_at_seminars/Exersice1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exersice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter first variable: ");
+                int variable1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second variable: ");
+                int variable2 = int.Parse(Console.ReadLine());
+
+                string test = VariableReplace(variable1, variable2);
+                Console.WriteLine(test);
+            }
+        }
+
+        static string VariableReplace(int firstVar, int secondVar)
+        {
+            int tempVar = firstVar;
+            firstVar = secondVar;
+            secondVar = tempVar;
+
+            string result = "Now first variable is " + firstVar + " and second variable is " + secondVar;
+
+            return result;
+        }
+    }
+
+}
