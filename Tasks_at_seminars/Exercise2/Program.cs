2bc379b8e2c7722d744ed485cc5e475278176452
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-13 16:32:56 +0300

    Add Exercise 2 from seminars tasks

---
+++ Tasks_at_seminars/Exercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = ReverseValue(234);
+            Console.WriteLine(test);
+        }
+
+        static int ReverseValue(int value)
+        {
+            int revCount = 100;
+            int revValue = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                revValue += ((value % 10) * revCount);
+                revCount /= 10;
+                value /= 10;
+            }
+            return revValue;
+        }
+    }
+}
