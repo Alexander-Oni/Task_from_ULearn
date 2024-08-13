066884c502952b247fdfea0dd0f7d08f7f051069
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-13 15:59:44 +0300

    Add code of FindSignAngle method

--- AngryBirds/AngryBirdsTask.cs
+++ AngryBirds/AngryBirdsTask.cs
@@ -5,7 +5,9 @@ namespace AngryBirds;
 public static class AngryBirdsTask
 {
     public static double FindSightAngle(double v, double distance)
-    {
-        return Math.PI / 4;
+    {    
+        const double G = 9.8;
+
+        return 0.5 * Math.Asin((distance * G) / Math.Pow(v, 2));
     }
 }
