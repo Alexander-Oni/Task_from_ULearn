3867c96ac6f63be060a4de175b6a81b4efbc78db
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-14 11:43:15 +0300

    Add CalculatePersentPfDeposit method in PersentCount

---
+++ PercentCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static double CalculatePersentOFDeposit(string stringValue)
+        {
+            double[] resultValue = stringValue.Split(' ').Select(double.Parse).ToArray();
+
+            double deposit = resultValue[0];
+            double depositPersent = resultValue[1];
+            double month = resultValue[2];
+
+            return deposit * Math.Pow((1 + depositPersent / (12 * 100)), month);
+        }
+    }
+}
+
