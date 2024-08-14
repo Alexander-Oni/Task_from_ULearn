501360ba022fcb41cefd935a035ca585fb924f18
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-14 13:31:31 +0300

    Add AngleBetweenHoursAndMinutes method in Exercise1

---
+++ Tasks_at_seminars/Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AngleBetweenHoursAndMinutes(5));
+        }
+
+        static int AngleBetweenHoursAndMinutes(int hours)
+        {
+            return hours % 12 * 30;
+        }
+    }
+}
