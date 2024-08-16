f7060e27c64f7395b841e626f22c2ed405363caf
Alexander-Oni <alex.evant1448@gmail.com>
2024-08-16 13:04:43 +0300

    Add PluralizeRub method in Pluralizer

---
+++ Conditional_constructions/Practices/The_decline_of_the_ruble/PluralizeTask.cs
@@ -0,0 +1,19 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+		int lastNumber = count % 10;
+		int lastTwoNumber = count % 100;
+
+		if (lastNumber == 1 && lastTwoNumber != 11)
+			return "рубль";
+		else if ((lastNumber == 2 || lastNumber == 3 || lastNumber == 4) &&
+				(lastTwoNumber != 12 || lastTwoNumber != 13 || lastTwoNumber != 14))
+			return "рубля";
+		else
+			return "рублей";
+	}
+}
