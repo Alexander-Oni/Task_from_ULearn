6b5a5a676b28ddb31112f5cc116cc53311e10064
Alexander-Oni <alex.evant1448@gmail.com>
2025-02-05 21:41:39 +0300

    Refactored Exercise2 in First_meet

--- The_basics_of_programmingC#._Part1/Tasks_at_seminars/First_meet/Exercise2/Program.cs
+++ The_basics_of_programmingC#._Part1/Tasks_at_seminars/First_meet/Exercise2/Program.cs
@@ -10,16 +10,29 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            int test = ReverseValue(234);
-            Console.WriteLine(test);
+            int test1 = ReverseValue(123456789);
+            Console.WriteLine(test1);
+
+            int test2 = ReverseValue(12345678);
+            Console.WriteLine(test2);
+
+            int test3 = ReverseValue(1234567);
+            Console.WriteLine(test3);
+
+            int test4 = ReverseValue(123456);
+            Console.WriteLine(test4);
+
+            int test5 = ReverseValue(12345);
+            Console.WriteLine(test5);
         }
 
         static int ReverseValue(int value)
         {
-            int revCount = 100;
+            int countOfNumber = value.ToString().Length;
+            int revCount = (int)Math.Pow(10, (countOfNumber - 1));
             int revValue = 0;
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < countOfNumber; i++)
             {
                 revValue += ((value % 10) * revCount);
                 revCount /= 10;
