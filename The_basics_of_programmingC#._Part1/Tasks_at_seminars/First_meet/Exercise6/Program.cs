5eb557ff974fccf9b7e3d4e88e8b8f9320510818
Alexander-Oni <alex.evant1448@gmail.com>
2025-02-07 19:28:41 +0300

    Add dicision of Exercise6 in First_meet

---
+++ The_basics_of_programmingC#._Part1/Tasks_at_seminars/First_meet/Exercise6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] test = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            Console.WriteLine(DistanceToLine(test));
+
+            int[,] test1 = new int[,] { { 1, 2 }, { 3, 4 }, { 6, 8 } };
+            Console.WriteLine(DistanceToLine(test1));
+
+            int[,] test2 = new int[,] { { 10, 0 }, { 0, 0 }, { 8, 6 } };
+            Console.WriteLine(DistanceToLine(test2));
+
+            int[,] test3 = new int[,] { { 8, 0 }, { 0, 0 }, { 6, 2 } };
+            Console.WriteLine(DistanceToLine(test3));
+        }
+        static double DistanceToLine(int[,] coordinates)
+        {
+            double x0 = coordinates[0,0];  //   
+            double y0 = coordinates[0,1];  //   координаты точки
+            double x1 = coordinates[1,0];  //
+            double y1 = coordinates[1,1];  //   координаты точки на прямой
+            double x2 = coordinates[2,0];  //
+            double y2 = coordinates[2,1];  //   координаты точки на прямой
+
+            double A = y2 - y1;
+            double B = x2 - x1;
+            double C = A * -x1 + B * y1;
+
+            double distance = (Math.Abs(A * x0 - B * y0 + C)) / Math.Sqrt(A * A + B * B);
+
+            return distance;
+        }
+    }
+}
