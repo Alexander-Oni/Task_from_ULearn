f1f8f6b2d6ebceac325d0a73fb2c70704337b806
Alexander-Oni <alex.evant1448@gmail.com>
2025-02-08 11:55:39 +0300

    Add dicision of Exercise7 in First_meet

---
+++ The_basics_of_programmingC#._Part1/Tasks_at_seminars/First_meet/Exercise7/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 1, 2, 3 };
+            PrintCoordinates(FindTheGuiding(test));
+            PrintCoordinates(FindTheNormal(test));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int[] test1 = { 4, -7, 8 };
+            PrintCoordinates(FindTheGuiding(test1));
+            PrintCoordinates(FindTheNormal(test1));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int[] test2 = { 5, -5, 3 };
+            PrintCoordinates(FindTheGuiding(test2));
+            PrintCoordinates(FindTheNormal(test2));
+            Console.WriteLine(); 
+            Console.WriteLine();
+        }
+
+        static int[] FindTheGuiding(int[] coefficients)
+        {
+            int x = coefficients[1];
+            int y = -coefficients[0];
+
+            int[] result = { x, y };
+            return result;
+        }
+
+        static int[] FindTheNormal(int[] coefficients)
+        {
+            int x = coefficients[0];
+            int y = coefficients[1];
+
+            int[] result = { x, y };
+            return result;
+        }
+
+        static void PrintCoordinates(int[] coordinates)
+        {
+            for (int i = 0; i < coordinates.Length; i++) 
+                Console.Write(coordinates[i] + " ");
+        }
+    }
+}
